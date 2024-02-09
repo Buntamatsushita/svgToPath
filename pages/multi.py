258eb748a52037213c86d67ace28b64cbe4fee51
Buntako <buntamatsushita@gmail.com>
2024-02-09 18:59:16 +0900

    複数ファイルに対応

--- pages/multi.py
+++ pages/multi.py
@@ -4,21 +4,21 @@ def parse_svg_properties(svg_text):
     svg_dict = {}
     svg_properties_start = svg_text.find("<svg") + 5
     svg_properties_end = svg_text.find(">")
-
+    
     if svg_properties_start != -1 and svg_properties_end != -1:
         svg_properties = svg_text[svg_properties_start:svg_properties_end]
         svg_properties = svg_properties.replace('" ', ',').replace('"', '')
         svg_dict = dict(item.split("=") for item in svg_properties.split(","))
-
+    
     return svg_dict
 
 
 def extract_path(svg_text):
     path_datas = []
     fills = []
-
+    
     times = svg_text.count("<path")
-
+    
     while times > 0:
         path_data = svg_text[svg_text.find("<path") + 5:svg_text.find("/>")]
         path_data = path_data[path_data.find("d=") + 3:]
@@ -38,7 +38,7 @@ def extract_path(svg_text):
 
     return path_datas, fills
 
-def generate_xaml(file_name, svg_properties, xaml_property_mapping, svg_properties_is_checked):
+def generate_xaml(xaml_code, file_name, svg_properties, xaml_property_mapping, svg_properties_is_checked):
     xaml_code = f'<Style x:Key="{file_name}" TargetType="Path">\n'
     for prop_name, prop_value in svg_properties.items():
         if prop_name in xaml_property_mapping and prop_value != "none" and prop_name in svg_properties_is_checked:
@@ -56,64 +56,57 @@ def generate_xaml(file_name, svg_properties, xaml_property_mapping, svg_properti
                         xaml_code += f'    <Setter Property="{"Fill"}" Value="{svg_properties["fill"][i]}"/>\n'
             else:
                 xaml_code += f'    <Setter Property="{xaml_property_mapping[prop_name]}" Value="{prop_value}"/>\n'
-    xaml_code += '</Style>'
+    xaml_code += '</Style>\n'
     return xaml_code
 
 def main():
     st.title("SVG to XAML")
-
-    input_svgs = []
+    input_svg = ""
     result_xaml = ""
-    file_names = []
+    files = {}
 
     ignore_properties = ["xmlns", "class"]
 
-    xaml_propaty = {"width":"Width","height":"Height","fill":"Fill","stroke":"Stroke","stroke-width":"StrokeThickness","d":"Data","stroke-linecap":"StrokeLineCap","stroke-linejoin":"StrokeLineJoin","stroke-miterlimit":"StrokeMiterLimit","stroke-dasharray":"StrokeDashArray","stroke-dashoffset":"StrokeDashOffset","stroke-opacity":"StrokeOpacity","fill-rule":"FillRule","font-family":"FontFamily","font-size":"FontSize","font-weight":"FontWeight","font-style":"FontStyle","text-anchor":"TextAlignment","text-decoration":"TextDecorations","text-rendering":"TextRenderingHint","text-transform":"TextTransform","letter-spacing":"CharacterSpacing","word-spacing":"WordSpacing","writing-mode":"TextDirection","alignment-baseline":"BaselineAlignment","baseline-shift":"BaselineOffset","dominant-baseline":"BaselineAlignment","glyph-orientation-horizontal":"GlyphOrientation","glyph-orientation-vertical":"GlyphOrientation"}
-
+    xaml_propaty = {"width":"Width","height":"Height","fill":"Fill","stroke":"Stroke","stroke-width":"StrokeThickness","data":"Data","stroke-linecap":"StrokeLineCap","stroke-linejoin":"StrokeLineJoin","stroke-miterlimit":"StrokeMiterLimit","stroke-dasharray":"StrokeDashArray","stroke-dashoffset":"StrokeDashOffset","stroke-opacity":"StrokeOpacity","fill-rule":"FillRule","font-family":"FontFamily","font-size":"FontSize","font-weight":"FontWeight","font-style":"FontStyle","text-anchor":"TextAlignment","text-decoration":"TextDecorations","text-rendering":"TextRenderingHint","text-transform":"TextTransform","letter-spacing":"CharacterSpacing","word-spacing":"WordSpacing","writing-mode":"TextDirection","alignment-baseline":"BaselineAlignment","baseline-shift":"BaselineOffset","dominant-baseline":"BaselineAlignment","glyph-orientation-horizontal":"GlyphOrientation","glyph-orientation-vertical":"GlyphOrientation"}
+    xaml_proparty_required = ["width", "height", "data"]
     choice = st.radio("Select input type", ("text", "svg file"))
 
     if choice == "text":
-        num_svgs = st.number_input("Enter the number of SVGs", min_value=1, value=1, step=1)
-        for i in range(num_svgs):
-            input_svg = st.text_area(f"Enter SVG {i+1}", "")
-            input_svgs.append(input_svg)
+        input_svg = st.text_area("Enter svg", "")
     elif choice == "svg file":
-        num_svgs = st.number_input("Enter the number of SVG files", min_value=1, value=1, step=1)
-        for i in range(num_svgs):
-            uploaded_file = st.file_uploader(f"Upload SVG file {i+1}", type=['svg'])
-            if uploaded_file is not None:
-                input_svg = uploaded_file.read().decode("utf-8")
-                input_svgs.append(input_svg)
-                file_name = uploaded_file.name
-                file_name = file_name[:file_name.find(".")]
-                file_names.append(file_name)
-                st.code(input_svg, "svg")
+        uploaded_file = st.file_uploader("Upload svg file", type=['svg'],accept_multiple_files=True)
+        for i in uploaded_file:
+            input_svg = i.read().decode("utf-8")
+            files[i.name] = input_svg
 
-    if input_svgs:
-        for i, input_svg in enumerate(input_svgs):
-            # SVGの解析
-            svg_properties = parse_svg_properties(input_svg)
+    if files != "":
+        svg_properties_is_checked = st.multiselect("xamlに変換する要素を選択してください。", list(xaml_propaty.keys()), list(xaml_proparty_required))
+        change = False
+        if change != st.button("convert"):
+            # XAMLコードの生成
+            change = not change
+            for file in files:
+                input_svg = files[file]
+                file_name = file
+                # SVGの解析
+                svg_properties = parse_svg_properties(input_svg)
 
-            # パスの抽出
-            path_datas, fills = extract_path(input_svg)
-            svg_properties["d"] = path_datas
-            svg_properties["fill"] = fills
+                # パスの抽出
+                path_datas, fills = extract_path(input_svg)
+                svg_properties["d"] = path_datas
+                svg_properties["fill"] = fills
 
-            show_xaml = st.checkbox(f"Show XAML for SVG {i+1}", False)
-            if show_xaml:
-                st.write(svg_properties)
+                show_xaml = st.checkbox("xamlの要素を見る", False)
+                if show_xaml:
+                    st.write(svg_properties)
 
-            for j in ignore_properties:
-                if j in svg_properties:
-                    del svg_properties[j]
-            svg_properties_is_checked = st.multiselect(f"Select XAML properties to convert for SVG {i+1}", list(svg_properties.keys()), list(svg_properties.keys()))
-            change = False
-            if change != st.button(f"Convert SVG {i+1}"):
-                # XAMLコードの生成
-                change = not change
-                result_xaml += generate_xaml(file_names[i], svg_properties, xaml_propaty, svg_properties_is_checked)
+                for i in ignore_properties:
+                    if i in svg_properties:
+                        del svg_properties[i]
 
-    st.subheader("Result (XAML)")
+                    result_xaml = generate_xaml(result_xaml, file_name, svg_properties, xaml_propaty, svg_properties_is_checked)
+
+    st.subheader("result(xaml)")
     st.code(result_xaml, "xaml")
 
 if __name__ == "__main__":
